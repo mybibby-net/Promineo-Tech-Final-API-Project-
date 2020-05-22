a7b83dfc9bbc1e26777356e01f28e3e480ae40d3
Tristan Chester <tristanlchester@gmail.com>
2020-05-22 16:34:20 -0700

    add getters and setters

--- src/main/java/com/tristanchester/npc/entity/NPC.java
+++ src/main/java/com/tristanchester/npc/entity/NPC.java
@@ -1,17 +1,51 @@
 package com.tristanchester.npc.entity;
 
+import com.tristanchester.npc.util.NpcLevel;
+
 public class NPC {
 
+	private Long id;
+	private String name;
+	private int age;
+//	private NpcLevel npcLevel;
+//	private Location currentLocation;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 }
 /* 
  * NPC Properties: 
  *	
  *	Name: String
  *	NpcLevel: Enum
- *	Homeland: String
+ *	Homeland: Location object
  *	Current Location: String
  *	Age: int
- *  Stats:
+ *  Stats: //TODO plan logic for Stats and modifiers
  *    Strength: int
  *    Stamina: int
  *    Dexterity: int
