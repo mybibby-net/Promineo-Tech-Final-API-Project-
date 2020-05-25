a5f67b622e72de4d530eb3e6652e65dd4bf7011e
Tristan Chester <tristanlchester@gmail.com>
2020-05-24 17:14:22 -0700

    add Location properties

--- src/main/java/com/tristanchester/npc/entity/Location.java
+++ src/main/java/com/tristanchester/npc/entity/Location.java
@@ -2,6 +2,43 @@ package com.tristanchester.npc.entity;
 
 public class Location {
 
+	private String name;
+	private String description;
+	private int age;
+	private int size;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
 }
 /*
  * Location properties:
@@ -9,5 +46,4 @@ public class Location {
  *	 LocationDescription: String
  *	 LocationAge: int
  *	 SizeInKm: int
- *	 
  */
