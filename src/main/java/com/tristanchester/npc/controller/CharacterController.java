5e479f63ff219798acb2276609255426df07b464
Tristan Chester <tristanlchester@gmail.com>
2020-05-28 16:51:28 -0700

    minor code placement adjustment

--- src/main/java/com/tristanchester/npc/controller/CharacterController.java
+++ src/main/java/com/tristanchester/npc/controller/CharacterController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tristanchester.npc.entity.Character;
@@ -18,8 +19,14 @@ public class CharacterController {
 	@Autowired
 	private CharacterService service;
 	
-	//TODO: add Request Mappings
-	//Request map GET
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Object> createCharacter(@RequestBody Character character) {
+		return new ResponseEntity<Object>(
+				service.createCharacter(character), HttpStatus.CREATED
+				);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<Object> getCharacter(@PathVariable Long id) {
 		try {
 			return new ResponseEntity<Object>(
@@ -32,21 +39,14 @@ public class CharacterController {
 		}
 	}
 	
-	//Request Map GET
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<Object> getAllCharacters() {
 		return new ResponseEntity<Object>(
 				service.getCharacters(), HttpStatus.OK
 				);
 	}
 	
-	//Request Map POST
-	public ResponseEntity<Object> createCharacter(@RequestBody Character character) {
-		return new ResponseEntity<Object>(
-				service.createCharacter(character), HttpStatus.CREATED
-				);
-	}
-	
-	//Request Map PUT (update)
+	@RequestMapping(method = RequestMethod.PUT)
 	public ResponseEntity<Object> modifyCharacter(@RequestBody Character character, @PathVariable Long id) throws Exception {
 		try {
 			return new ResponseEntity<Object>(
@@ -59,6 +59,7 @@ public class CharacterController {
 		}
 	}
 	
+	@RequestMapping(method = RequestMethod.DELETE)
 	public ResponseEntity<Object> removeCharacter(@PathVariable Long id) {
 		try {
 			service.removeCharacter(id);
