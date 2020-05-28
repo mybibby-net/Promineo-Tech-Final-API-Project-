fb37bd1ab8a2c12c8e3dea8a4ab2f4a5bead72b9
Tristan Chester <tristanlchester@gmail.com>
2020-05-28 16:59:04 -0700

    added RequestMappings, RestController annotation

--- src/main/java/com/tristanchester/npc/controller/ItemController.java
+++ src/main/java/com/tristanchester/npc/controller/ItemController.java
@@ -6,14 +6,15 @@ import com.tristanchester.npc.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Service
+@RestController
+@RequestMapping("/items")
 public class ItemController {
 	
 	@Autowired
