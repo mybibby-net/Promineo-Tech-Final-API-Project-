14ea838e2050ba6d7196e3735d0b830fe99d28d2
mybibby-net <tristanlchester@gmail.com>
2020-05-29 02:11:38 -0700

    added location controller, service

---
+++ src/main/java/com/tristanchester/npc/service/LocationService.java
@@ -0,0 +1,8 @@
+package com.tristanchester.npc.service;
+
+public class LocationService {
+	//TODO: write logic for calculation for population size based on characters with the same location
+
+	
+	
+}
