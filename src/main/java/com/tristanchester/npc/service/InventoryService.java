9557943d22b710c6cf7d254e10e23d91112a7601
Tristan Chester <tristanlchester@gmail.com>
2020-05-28 17:03:17 -0700

    added imports, Repo variables

--- src/main/java/com/tristanchester/npc/service/InventoryService.java
+++ src/main/java/com/tristanchester/npc/service/InventoryService.java
@@ -1,5 +1,57 @@
 package com.tristanchester.npc.service;
 
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.tristanchester.npc.entity.Character;
+import com.tristanchester.npc.entity.Inventory;
+import com.tristanchester.npc.entity.Item;
+import com.tristanchester.npc.repository.CharacterRepo;
+import com.tristanchester.npc.repository.InventoryRepo;
+import com.tristanchester.npc.repository.ItemRepo;
+
+@Service
 public class InventoryService {
 
+	private static final Logger logger = LogManager.getLogger(InventoryService.class);
+
+	private InventoryRepo repo;
+	
+	private CharacterRepo characterRepo;
+	
+	private ItemRepo itemRepo;
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
