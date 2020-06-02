e8379b1121ada0ec5c071b01029dc1d676f4511c
Tristan Chester <tristanlchester@gmail.com>
2020-06-02 15:24:20 -0700

    commented out old code

--- src/main/java/com/tristanchester/npc/service/InventoryService.java
+++ src/main/java/com/tristanchester/npc/service/InventoryService.java
@@ -19,7 +19,6 @@ import com.tristanchester.npc.repository.ItemRepo;
 public class InventoryService {
 
 	private static final Logger logger = LogManager.getLogger(InventoryService.class);
-	private final int inventorySize = 28;
 	
 	@Autowired
 	private InventoryRepo repo;
@@ -29,59 +28,66 @@ public class InventoryService {
 	
 	@Autowired
 	private ItemRepo itemRepo;
-	
-	//Initializing inventory with base values, removed Set<Item> param
-	public Inventory initializeNewEmptyInventory(Long ownerId) {
+
+	//TODO: consider prototyping a combination of initialization and submission hybrid method for create of new inventories?
+	//Call for POST Sets default values, associates character, doesn't set times.
+	public Inventory createNewInventory(Long ownerId) {
 		Inventory inventory = new Inventory();
 		inventory.setOwner(
-				characterRepo.findOne(ownerId)
-				);
-		inventory.setSize(inventorySize); //28 as default size, think "slots"
-		return inventory;
+				characterRepo.findOne(ownerId));
+		inventory.setSize(28); //28 as default size, think "slots"
+		inventory.setWorth(0);
+		inventory.setWeight(0);
+		return repo.save(inventory);
 	}
-	
-	public int getItemQuantity(Inventory inventory) {
-		
-		return 0;
-	}
-	
-	public Inventory submitNewInventory(Set<Long> itemIds, Long ownerId) {
-		try {
-			Inventory inventory = initializeNewEmptyInventory(ownerId);
-			return repo.save(inventory);
-		} catch (Exception e) {
-			logger.error(
-					"Exception occured while trying to create inventory for character with id: " + ownerId, e
-					);
-			throw e;
+
+//	TODO: uncomment if new method syntax fails
+	public Inventory addItems(Set<Long> itemIds, Long ownerId) throws Exception {
+		// Call for PUT Takes character inventory size int, checks if it's lower than the overall size of the inventory (default 28),
+		// then allows operation if meets condition, changed from if to while
+		if (itemIds.size() < characterRepo.findOne(ownerId).getInventory().getSize()) {
+			try {
+				Character owner = characterRepo.findOne(ownerId);
+				Inventory originalInventory = owner.getInventory();
+				originalInventory.setItems(
+						convertItemsToSet(itemRepo.findAll(itemIds)));
+				return originalInventory;
+			} catch (Exception e) {
+				logger.error(
+						"Unable to add items to inventory with id: " + ownerId, e);
+				throw new Exception("Unable to add to inventory");
+			}
+
+		} else {
+			return null; //TODO: cross fingers and hope this works
 		}
 	}
-	
-	public Set<Item> convertToItemSet(Iterable<Item> iterableItem) {
+
+	public Set<Item> convertItemsToSet(Iterable<Item> iterableItem) {
 		Set<Item> itemSet = new HashSet<Item>();
 		for (Item item : iterableItem) {
 			itemSet.add(item);
 		}
 		return itemSet;
 	}
-	
+	//Takes inventory, calls getItems() to retrieve all items, then calls size() to count all items and returns the number
+	public int getItemQuantity(Inventory inventory) { return inventory.getItems().size(); }
+
+	//TODO: Consider using Charisma as a discount enum
 	public int calculateNetWorth(Set<Item> items) {
-		int worth = 0; //Inventory Initial Value
+		int worth = 0; //Inventory worth Initial Value
 		for (Item item : items) {
 			worth += item.getCost();
 		}
 		return worth;
 	}
-	
+
+	//Used to shorten conditionals, returns remaining space (out of maximum of 28) as int
+	public int getSpaceRemaining(Inventory inventory) {
+		return inventory.getSize() - inventory.getItems().size();
+	}
+
 }
-/*
- *  Inventory Initialization:
- *    TODO: 
- *    		2. write system for weight calculation
- *    		3. write system for inventory weight 
- *    		   and physical space (size) limits
- *    		Use Iterable<Item> for these?
- */
 
 	
 
