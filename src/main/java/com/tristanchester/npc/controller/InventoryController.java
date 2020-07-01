e5cf80604d1a0101052fa383a8c65bb4d280fbdc
Tristan Chester <tristanlchester@gmail.com>
2020-07-01 11:43:10 -0700

    formatting changes

--- src/main/java/com/tristanchester/npc/controller/InventoryController.java
+++ src/main/java/com/tristanchester/npc/controller/InventoryController.java
@@ -2,6 +2,7 @@ package com.tristanchester.npc.controller;
 
 import java.util.Set;
 
+import com.tristanchester.npc.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tristanchester.npc.entity.Inventory;
 import com.tristanchester.npc.service.InventoryService;
 
-import javax.xml.ws.Response;
-
 @RestController
 @RequestMapping("characters/{id}/inventory") 
 public class InventoryController {
@@ -23,6 +22,7 @@ public class InventoryController {
 	@Autowired
 	private InventoryService service;
 
+	//Initializes inventory with specified set of items if there are any
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Object> createInventory (
 			@RequestBody Set<Long> itemIds, @PathVariable Long id) {
@@ -35,16 +35,24 @@ public class InventoryController {
 		}
 	}
 
-	@RequestMapping(value = "/{inventoryId}")
-	public ResponseEntity<Object> getInventory(@PathVariable Long id) {
+	//Retrieves inventory by owner id specified as path variable
+	@RequestMapping(method = RequestMethod.GET, value="/{inventoryId}")
+	public ResponseEntity<Object> getInventory(@PathVariable Long inventoryId) {
 		try {
-			return new ResponseEntity<Object>(service.getInventoryById(id), HttpStatus.OK);
+			return new ResponseEntity<Object>(service.getInventoryById(inventoryId), HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
 	}
 
-	//PUT: Adds set of item id's to inventory
+	@RequestMapping(value = "/{inventoryId}/items", method = RequestMethod.GET)
+	public ResponseEntity<Set<Item>> getItems(@PathVariable Long inventoryId) {
+		return new ResponseEntity<Set<Item>>(
+			service.getInventoryById(inventoryId).getItems(), HttpStatus.OK
+		);
+	}
+
+	//Adds set of item id's to inventory
 	@RequestMapping(value = "/{inventoryId}", method = RequestMethod.PUT)
 	public ResponseEntity<Object> updateInventory(
 			@RequestBody Set<Long> itemIds, @PathVariable Long inventoryId) {
@@ -56,6 +64,18 @@ public class InventoryController {
 					e.getMessage(), HttpStatus.BAD_REQUEST);
 			}
 		}
+
+	//Removes all items within an inventory
+	@RequestMapping(value = "/{inventoryId}",method = RequestMethod.DELETE)
+	public ResponseEntity<Object> deleteInventory(@PathVariable Long inventoryId) {
+		try {
+			service.deleteInventory(inventoryId);
+			return new ResponseEntity<Object>("Successfully deleted inventory with id" + inventoryId, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>("Unable to delete inventory with id:" + inventoryId, HttpStatus.BAD_REQUEST);
+		}
+	}
+
 }
 
 
