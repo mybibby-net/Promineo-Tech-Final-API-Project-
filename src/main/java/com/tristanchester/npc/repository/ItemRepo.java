2a97f36212ff44266fbcfe8fa6d88c655c387577
Tristan Chester <tristanlchester@gmail.com>
2020-07-01 11:48:55 -0700

    removed unecessary annotation

--- src/main/java/com/tristanchester/npc/repository/ItemRepo.java
+++ src/main/java/com/tristanchester/npc/repository/ItemRepo.java
@@ -3,9 +3,7 @@ package com.tristanchester.npc.repository;
 import com.tristanchester.npc.entity.Item;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ItemRepo extends CrudRepository<Item, Long>{
 
 }
